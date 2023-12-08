2444156558df7858766b357a281ba752ee71408e
Hamza Akhtar <hamzaakhtar8812@gmail.com>
2023-12-08 20:14:42 +0500

    pipelining is added in core (without hazard controlling)

--- processor.sv
+++ processor.sv
@@ -4,44 +4,74 @@ module processor
     input logic rst
 ); 
     // wires
+
     logic        rf_en;
-    logic        dm_en;
-
-    logic        sel_pc;
-    logic        sel_opr_a;
-    logic        sel_opr_b;
-    logic [ 1:0] sel_wb;
-
     logic [ 2:0] imm_type;
-
-    logic [31:0] pc_out;
-    logic [31:0] inst;
-    
     logic [ 4:0] rd;
     logic [ 4:0] rs1;
     logic [ 4:0] rs2;
-    
     logic [ 6:0] opcode;
     logic [ 2:0] funct3;
     logic [ 6:0] funct7;
-    
-    logic [ 3:0] aluop;
 
-    logic [31:0] wdata;
-    logic [31:0] rdata1;
-    logic [31:0] rdata2;
-    
-    logic [31:0] imm;
-    logic [31:0] opr_res;
-    
     logic [31:0] mux_out_pc;
     logic [31:0] mux_out_opr_a;
     logic [31:0] mux_out_opr_b;
 
-    logic [31:0] dmem_out;
+    logic        dm_en_id;
+    logic        dm_en_ex;
+    logic        dm_en_mem;
+
+    logic        sel_pc_if;
+    logic        sel_pc_id;
+
+    logic        sel_opr_a_id;
+    logic        sel_opr_a_ex;
+
+    logic        sel_opr_b_id;
+    logic        sel_opr_b_ex;
+
+    logic [ 1:0] sel_wb_id;
+    logic [ 1:0] sel_wb_ex;
+    logic [ 1:0] sel_wb_mem;
+    logic [ 1:0] sel_wb_wb;
+
+    logic [31:0] pc_out_if;
+    logic [31:0] pc_out_id;
+    logic [31:0] pc_out_ex;
+    logic [31:0] pc_out_mem;
+    logic [31:0] pc_out_wb;
+
+    logic [31:0] inst_if;
+    logic [31:0] inst_id;
+    
+    logic [ 3:0] aluop_id;
+    logic [ 3:0] aluop_ex;
+
+    logic [31:0] wdata_wb;
+    logic [31:0] wdata_id;
+
+    logic [31:0] rdata1_id;
+    logic [31:0] rdata1_ex;
+
+    logic [31:0] rdata2_id;
+    logic [31:0] rdata2_ex;
+    logic [31:0] rdata2_mem;
+    
+    logic [31:0] imm_id;
+    logic [31:0] imm_ex;
+
+    logic [31:0] opr_res_ex;
+    logic [31:0] opr_res_mem;
+    logic [31:0] opr_res_wb;
+
+    logic [31:0] dmem_out_mem;
+    logic [31:0] dmem_out_wb;
+
+    // --------------------- Instruction Fetch ---------------------
 
     // pc selection mux
-    assign mux_out_pc = sel_pc ? wdata : (pc_out + 32'd4);
+    assign mux_out_pc = sel_pc_if ? wdata_id : (pc_out_if + 32'd4);
 
     // program counter
     pc pc_i
@@ -49,20 +79,40 @@ module processor
         .clk   ( clk            ),
         .rst   ( rst            ),
         .pc_in ( mux_out_pc     ),
-        .pc_out( pc_out         )
+        .pc_out( pc_out_if      )
     );
 
     // instruction memory
     inst_mem inst_mem_i
     (
-        .addr  ( pc_out         ),
-        .data  ( inst           )
+        .addr  ( pc_out_if      ),
+        .data  ( inst_if        )
     );
+
+    // -------------------------------------------------------------
+
+
+    // IF <-> ID
+    always_ff @( posedge clk ) 
+    begin
+        if(rst)
+        begin
+            inst_id   <= 0;
+            pc_out_id <= 0;
+        end
+        else
+        begin
+            inst_id   <= inst_if;
+            pc_out_id <= pc_out_if;
+        end
+    end
+
+    // --------------------- Instruction Decode ---------------------
 
     // instruction decoder
     inst_dec inst_dec_i
     (
-        .inst  ( inst           ),
+        .inst  ( inst_id        ),
         .rs1   ( rs1            ),
         .rs2   ( rs2            ),
         .rd    ( rd             ),
@@ -79,69 +129,175 @@ module processor
         .rd    ( rd             ),
         .rs1   ( rs1            ),
         .rs2   ( rs2            ),
-        .rdata1( rdata1         ),
-        .rdata2( rdata2         ),
-        .wdata ( wdata          )
+        .rdata1( rdata1_id      ),
+        .rdata2( rdata2_id      ),
+        .wdata ( wdata_id       )
     );
 
     // controller
     controller controller_i
     (
-        .opcode    ( opcode         ),
-        .funct3    ( funct3         ),
-        .funct7    ( funct7         ),
-        .rf_en     ( rf_en          ),
-        .dm_en     ( dm_en          ),
-        .sel_opr_a ( sel_opr_a      ),
-        .sel_opr_b ( sel_opr_b      ),
-        .sel_pc    ( sel_pc         ),
-        .sel_wb    ( sel_wb         ),
-        .imm_type  ( imm_type       ),
-        .aluop     ( aluop          )
+        .opcode    ( opcode       ),
+        .funct3    ( funct3       ),
+        .funct7    ( funct7       ),
+        .rf_en     ( rf_en_id     ),
+        .dm_en     ( dm_en_id     ),
+        .sel_opr_a ( sel_opr_a_id ),
+        .sel_opr_b ( sel_opr_b_id ),
+        .sel_pc    ( sel_pc_id    ),
+        .sel_wb    ( sel_wb_id    ),
+        .imm_type  ( imm_type     ),
+        .aluop     ( aluop_id     )
     );
 
     // immediate generator
     imm_gen imm_gen_i
     (
-        .inst      ( inst           ),
+        .inst      ( inst_id        ),
         .imm_type  ( imm_type       ),
-        .imm       ( imm            )
+        .imm       ( imm_id         )
     );
 
+    // -------------------------------------------------------------
+
+
+    // ID <-> EX
+    always_ff @( posedge clk ) 
+    begin
+        if(rst)
+        begin
+            pc_out_ex    <= 0;
+            rdata1_ex    <= 0;
+            rdata2_ex    <= 0;
+            imm_ex       <= 0;
+
+            // control signals
+            dm_en_ex     <= 0;
+            sel_opr_a_ex <= 0;
+            sel_opr_b_ex <= 0;
+            sel_wb_ex    <= 0;
+            aluop_ex     <= 0;
+
+        end
+        else
+        begin
+            pc_out_ex    <= pc_out_id;
+            rdata1_ex    <= rdata1_id;
+            rdata2_ex    <= rdata2_id;
+            imm_ex       <= imm_id;
+            
+            // control signals
+            dm_en_ex     <= dm_en_id;
+            sel_opr_a_ex <= sel_opr_a_id;
+            sel_opr_b_ex <= sel_opr_b_id;
+            sel_wb_ex    <= sel_wb_id;
+            aluop_ex     <= aluop_id;
+        end
+    end
+
+    // Feedback from ID to IF
+    always_comb
+    begin
+        sel_pc_if = sel_pc_id;
+    end
+
+    // --------------------- Execute ---------------------
+
     // operand a selection mux
-    assign mux_out_opr_a = sel_opr_a ? pc_out : rdata1;
+    assign mux_out_opr_a = sel_opr_a_ex ? pc_out_ex : rdata1_ex;
 
     // operand b selection mux
-    assign mux_out_opr_b = sel_opr_b ? imm    : rdata2;
+    assign mux_out_opr_b = sel_opr_b_ex ?    imm_ex : rdata2_ex;
 
     // alu
     alu alu_i
     (
-        .aluop   ( aluop          ),
+        .aluop   ( aluop_ex       ),
         .opr_a   ( mux_out_opr_a  ),
         .opr_b   ( mux_out_opr_b  ),
-        .opr_res ( opr_res        )
+        .opr_res ( opr_res_ex     )
     );
+
+    // -------------------------------------------------------------
+
+    // EX <-> MEM
+    always_ff @( posedge clk ) 
+    begin
+        if(rst)
+        begin
+            pc_out_mem  <= 0;
+            opr_res_mem <= 0;
+            rdata2_mem  <= 0;
+
+            // control signals
+            dm_en_mem   <= 0;
+            sel_wb_mem  <= 0;
+        end
+        else
+        begin
+            pc_out_mem  <= pc_out_ex;
+            opr_res_mem <= opr_res_ex;
+            rdata2_mem  <= rdata2_ex;
+
+            // control signals
+            dm_en_mem   <= dm_en_ex;
+            sel_wb_mem  <= sel_wb_ex;
+        end
+    end
+
+    // ----------------------- Memory ------------------------
 
     // data memory
     data_mem data_mem_i
     (
         .clk      ( clk           ),
-        .dm_en    ( dm_en         ),
-        .addr     ( wdata         ),
-        .data_in  ( rdata2        ),
-        .data_out ( dmem_out      )
+        .dm_en    ( dm_en_mem     ),
+        .addr     ( opr_res_mem   ),
+        .data_in  ( rdata2_mem    ),
+        .data_out ( dmem_out_mem  )
     );
+
+    // ---------------------------------------------------------
+
+    // MEM <-> WB
+    always_ff @( posedge clk ) 
+    begin
+        if(rst)
+        begin
+            pc_out_wb   <= 0;
+            opr_res_wb  <= 0;
+            dmem_out_wb <= 0;
+
+            sel_wb_wb   <= 0;
+        end
+        else
+        begin
+            pc_out_wb   <= pc_out_mem;
+            opr_res_wb  <= opr_res_mem;
+            dmem_out_wb <= dmem_out_mem;
+            sel_wb_wb   <= sel_wb_mem;
+        end
+    end
+
+    // --------------------- Write Back ---------------------
 
     // write back selection mux
     always_comb
     begin
-        case(sel_wb)
-            2'b00: wdata = opr_res;
-            2'b01: wdata = dmem_out;
-            2'b10: wdata = pc_out + 32'd4;
-            2'b11: wdata = 32'd0;
+        case(sel_wb_wb)
+            2'b00: wdata_wb = opr_res_wb;
+            2'b01: wdata_wb = dmem_out_wb;
+            2'b10: wdata_wb = pc_out_wb + 32'd4;
+            2'b11: wdata_wb = 32'd0;
         endcase
+    end
+
+    // -------------------------------------------------------------
+
+    // Feedback from WB to ID
+    always_comb
+    begin
+        wdata_id = wdata_wb;
     end
     
 endmodule
